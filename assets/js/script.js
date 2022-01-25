949e9a0ed84e413fc0dced908aa9fab3e2053ea0
Diego Kaku <diegokaku@gmail.com>
2022-01-25 02:29:16 -0600

    fail to simple save 1 value, feeling terrible, almost 3am, Am I that dumb?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,24 @@
 // current day and time
-let currentTime = moment().format("dddd, MMMM Do YYYY, HH:mm:ss");
-$("#currentDay").text("Today is: " + currentTime);
+let update = function () {
+  let currentTime = moment().format("dddd, MMMM Do YYYY, HH:mm:ss");
+  $("#currentDay").text("Today is: " + currentTime);
+  // auto refresh time
+  setInterval(update, 1000);
+};
 
-// function click save
-$(document).ready(function () {});
-
-$(".save-icon").click(function () {
-  console.log("click");
-  let myTask = $("input[name=taskToDo]").val();
-  $("input[name=taskToSo]").val("");
-  localStorage.setItem(myTask, "mytask");
+$(document).ready(function () {
+  update();
 });
 
-function testTest() {
-  $("textarea").value;
-  console.log("testTest");
-}
+// select textarea and input
+let saveButton = document.querySelector("#save-button");
+let userInputElement = document.querySelector("#user-input");
+// get value from input element
+let taskText = userInputElement.value.trim();
+let taskInput = $("input[name=taskToDo]").val();
+
+$("button").click(function (e) {
+  e.preventDefault();
+  localStorage.setItem("hour", "taskInput");
+  console.log(taskText, taskInput);
+});
