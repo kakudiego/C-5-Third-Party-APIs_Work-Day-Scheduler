84fcd35573e6d4a18cfb405ee0e8c8912c261bfe
Diego Kaku <diegokaku@gmail.com>
2022-01-25 22:25:04 -0600

    cant getItem, data in localstorage, stuck again

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,6 @@ $(document).ready(function () {
   update();
 });
 
-// variables
-let tasks = [];
-
 // select textarea and input
 let save1 = document.querySelector("#save-1");
 let save2 = document.querySelector("#save-2");
@@ -40,78 +37,100 @@ save1.addEventListener("click", function (event) {
   event.preventDefault();
   localStorage.setItem("save1", JSON.stringify(userInput1.value));
 
-  console.log(userInput.value);
+  console.log(userInput1.value);
   console.log("09:00");
 });
 save2.addEventListener("click", function () {
   localStorage.setItem("save2", JSON.stringify(userInput2.value));
 
-  console.log(userInput.value);
+  console.log(userInput2.value);
   console.log("10:00");
 });
 save3.addEventListener("click", function () {
   localStorage.setItem("save3", JSON.stringify(userInput3.value));
 
-  console.log(userInput.value);
+  console.log(userInput3.value);
   console.log("11:00");
 });
 save4.addEventListener("click", function () {
   localStorage.setItem("save4", JSON.stringify(userInput4.value));
 
-  console.log(userInput.value);
+  console.log(userInput4.value);
   console.log("12:00");
 });
 save5.addEventListener("click", function () {
   localStorage.setItem("save5", JSON.stringify(userInput5.value));
-  console.log(userInput.value);
+  console.log(userInput5.value);
 
   console.log("13:00");
 });
 save6.addEventListener("click", function () {
-  localStorage.setItem("save1", JSON.stringify(userInput6.value));
+  localStorage.setItem("save6", JSON.stringify(userInput6.value));
 
-  console.log(userInput.value);
+  console.log(userInput6.value);
   console.log("14:00");
 });
 save7.addEventListener("click", function () {
-  localStorage.setItem("save1", JSON.stringify(userInput7.value));
-  console.log(userInput.value);
-  localStorage.setItem("save7", userInput);
+  localStorage.setItem("save7", JSON.stringify(userInput7.value));
+
+  console.log(userInput7.value);
   console.log("15:00");
 });
 save8.addEventListener("click", function () {
-  localStorage.setItem("save1", JSON.stringify(userInput8.value));
+  localStorage.setItem("save8", JSON.stringify(userInput8.value));
 
-  console.log(userInput.value);
+  console.log(userInput8.value);
   console.log("16:00");
 });
 save9.addEventListener("click", function () {
-  localStorage.setItem("save1", JSON.stringify(userInput9.value));
+  localStorage.setItem("save9", JSON.stringify(userInput9.value));
 
-  console.log(userInput.value);
-  localStorage.setItem("save9", userInput);
+  console.log(userInput9.value);
   console.log("17:00");
 });
 
-// // load tasks
-// let loadTasks = function () {
-//   let savedTasks = localStorage.getItem("task");
+// load tasks
+// let dayTask = JSON.parse(localStorage.getItem("save4") || "{}");
+// console.log(dayTask["save1"]);
 
-//   if (!tasks) {
-//     tasks = [];
-//     return false;
-//   }
+console.log(localStorage);
 
-//   tasks = JSON.parse(tasks);
-// };
+var loadTasks = function () {
+  var savedTasks = localStorage.getItem("save1");
+  // if there are no tasks, set tasks to an empty array and return out of the function
+  if (!savedTasks) {
+    return false;
+  }
+  console.log("Saved tasks found!");
+  // else, load up saved tasks
 
-// To get data from local storage, you do something like this:
-// //get current data from localStorage. If no data exists, return empty object
-//     var dailySchedule = JSON.parse(localStorage.getItem("dailySchedule") || "{}");
+  // parse into array of objects
+  savedTasks = JSON.parse(savedTasks);
+
+  var savedTasks = localStorage.getItem("save2");
+  // if there are no tasks, set tasks to an empty array and return out of the function
+  if (!savedTasks) {
+    return false;
+  }
+  console.log("Saved tasks found!");
+  // else, load up saved tasks
+
+  // parse into array of objects
+  savedTasks = JSON.parse(savedTasks);
+};
+loadTasks();
+
+// // To get data from local storage, you do something like this:
+// // get current data from localStorage. If no data exists, return empty object
+// let dailySchedule = JSON.parse(localStorage.getItem("dailySchedule") || "{}");
+
+// // You can access these values just like you would any other object
+// console.log(dailySchedule["save1"]);
 
 // var dailySchedule will look like this (I think):
-// var dailySchedule = {"save1": "test1",
-//                       "save2": "test2",
-//                       "save3": "test3"}
-// You can access these values just like you would any other object
-// console.log(dailySchedule['save1']}
+// let dailySchedule =
+//   {
+//     "save1": "test1",
+//     "save2": "test2",
+//     "save3": "test3"
+//   }
